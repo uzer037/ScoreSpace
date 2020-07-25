4e244cccfdd946f1a4a6a82c2ee599f6fc060aee
sergeyrid0 <sergeyrid@yandex.ru>
2020-07-25 21:13:03 +0300

    avalanche prototype

---
+++ Assets/Scripts/Avalanche.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Avalanche : MonoBehaviour
+{
+    public UnityEvent chanceChanged;
+    public float width = 0.5f;
+    public int spread = 10;
+    public int count = 3;
+    public float chance = 0;
+    public GameObject player;
+    int[] avalanches;
+
+    void Start()
+    {
+        avalanches = new int[3];
+        CreateAvalanches();
+    }
+
+    public void ProcessTurn()
+    {
+        if (UnityEngine.Random.Range(0, 99) < chance)
+        {
+            LaunchAvalanches();
+            CreateAvalanches();
+            chance = 0;
+        }
+        chance += UnityEngine.Random.Range(0, 100);
+        if (chance > 100)
+        {
+            chance = 100;
+        }
+        chanceChanged.Invoke();
+    }
+
+    void LaunchAvalanches()
+    {
+        Debug.Log("OMG AVALANCHES!1!!11!");
+        foreach (int i in avalanches)
+        {
+            CheckHit(i);
+        }
+    }
+
+    void CreateAvalanches()
+    {
+        avalanches = Enumerable.Range(-spread, 2 * spread + 1).
+                     OrderBy(x => System.Guid.NewGuid()).Take(count).ToArray();
+        foreach (int i in avalanches)
+        {
+            Debug.Log(i);
+        }
+    }
+
+    void CheckHit(int pos)
+    {
+        float playerX = player.transform.position.x;
+        if (playerX > pos * width - width / 2 && playerX < pos * width + width / 2)
+        {
+            Debug.Log("HIT YA");
+        };
+    }
+}
--- Assets/Scripts/Chance.cs
+++ Assets/Scripts/Chance.cs
@@ -5,21 +5,17 @@ using UnityEngine.UI;
 
 public class Chance : MonoBehaviour
 {
-    int chance = 0;
     Text chanceText;
+    public GameObject controller;
+
     void Start()
     {
         chanceText = GetComponentsInChildren<Text>()[0];
-        chanceText.text = chance.ToString() + "%";
+        chanceText.text = controller.GetComponent<Avalanche>().chance.ToString() + "%";
     }
     
     public void ChangeChance()
     {
-        chance += Random.Range(0, 100);
-        if (chance >= 100)
-        {
-            chance -= 100;
-        }
-        chanceText.text = chance.ToString() + "%";
+        chanceText.text = controller.GetComponent<Avalanche>().chance.ToString() + "%";
     }
 }
