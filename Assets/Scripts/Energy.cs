8f70dc8a8f1f0099bce660ae43cef53a88caab26
sergeyrid0 <sergeyrid@yandex.ru>
2020-07-25 14:31:05 +0300

    energy

---
+++ Assets/Scripts/Energy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Energy : MonoBehaviour
+{
+    float energy = 0;
+    public float deltaEnergy = 0.01f;
+    RectTransform energyBar;
+    RectTransform spentEnergy;
+    bool full = false;
+    void Start()
+    {
+        energyBar = GetComponentsInChildren<RectTransform>()[2];
+        spentEnergy = GetComponentsInChildren<RectTransform>()[3];
+    }
+
+    void FixedUpdate()
+    {
+        spentEnergy.sizeDelta = new Vector2(energyBar.rect.width * energy, spentEnergy.rect.height);
+        energy += deltaEnergy;
+        if (energy > 1)
+        {
+            full = true;
+            energy = 1;
+        }
+    }
+}
